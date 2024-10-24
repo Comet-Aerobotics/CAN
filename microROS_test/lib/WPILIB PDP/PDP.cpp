753955c6c2f7d1d467dc44bbd1d4b92f687545b5
Matthew Krzewinski <134571920+coolguy157@users.noreply.github.com>
2024-10-23 20:37:27 -0500

    Add files via upload

--- microROS_test/lib/WPILIB PDP/PDP.cpp
+++ microROS_test/lib/WPILIB PDP/PDP.cpp
@@ -1,4 +1,4 @@
-#include "WPILIB_PDP.h"
+#include "PDP.h"
 #include <Comet_CAN_Helper.h>
 
 
@@ -7,14 +7,9 @@
 ** Descriptions:            Function to parse PDP Periodic Status Frame 1
 *********************************************************************************************************/
 void PDP::parse_status_frame_1(uint8_t * data, uint8_t size){
-  //if you're reading this i'm so sorry
-  status.currents[0] = (static_cast<uint32_t>(data[0]) << 2) | (data[1] & ((1 << 2) - 1));
-  //no, it doesn't get better
-  status.currents[1] = (static_cast<uint32_t>(data[1] >> (8-6)) << 4) | (data[2] & ((1 << 4) - 1));
-  status.currents[2] = (static_cast<uint32_t>(data[2] >> (8-4)) << 6) | (data[3] & ((1 << 6) - 1));
-  status.currents[3] = (static_cast<uint32_t>(data[3] >> (8-2)) << 6) | data[4];
-  status.currents[4] = (static_cast<uint32_t>(data[5]) << 2) | (data[6] & ((1 << 2) - 1));
-  status.currents[5] = (static_cast<uint32_t>(data[6] >> (8-6)) << 4) | (data[7] & ((1 << 4) - 1));
+  for (int i = 0; i <= 5; i++){
+    status.currents[i] = data[i] * kCurrentScalar;
+  }
 }
 
 /*********************************************************************************************************
@@ -22,14 +17,9 @@ void PDP::parse_status_frame_1(uint8_t * data, uint8_t size){
 ** Descriptions:            Function to parse PDP Periodic Status Frame 2
 *********************************************************************************************************/
 void PDP::parse_status_frame_2(uint8_t * data, uint8_t size){
-  //if you're reading this i'm so sorry
-  status.currents[6] = (static_cast<uint32_t>(data[0]) << 2) | (data[1] & ((1 << 2) - 1));
-  //no, it doesn't get better
-  status.currents[7] = (static_cast<uint32_t>(data[1] >> (8-6)) << 4) | (data[2] & ((1 << 4) - 1));
-  status.currents[8] = (static_cast<uint32_t>(data[2] >> (8-4)) << 6) | (data[3] & ((1 << 6) - 1));
-  status.currents[9] = (static_cast<uint32_t>(data[3] >> (8-2)) << 6) | data[4];
-  status.currents[10] = (static_cast<uint32_t>(data[5]) << 2) | (data[6] & ((1 << 2) - 1));
-  status.currents[11] = (static_cast<uint32_t>(data[6] >> (8-6)) << 4) | (data[7] & ((1 << 4) - 1));
+  for (int i = 6; i <= 11; i++){
+    status.currents[i] = data[i-6] * kCurrentScalar;
+  }
 }
 
 /*********************************************************************************************************
@@ -37,12 +27,12 @@ void PDP::parse_status_frame_2(uint8_t * data, uint8_t size){
 ** Descriptions:            Function to parse PDP Periodic Status Frame 3
 *********************************************************************************************************/
 void PDP::parse_status_frame_3(uint8_t * data, uint8_t size){
-  status.currents[12] = (static_cast<uint32_t>(data[0]) << 2) | (data[1] & ((1 << 2) - 1));
-  //no, it doesn't get better
-  status.currents[13] = (static_cast<uint32_t>(data[1] >> (8-6)) << 4) | (data[2] & ((1 << 4) - 1));
-  status.currents[14] = (static_cast<uint32_t>(data[2] >> (8-4)) << 6) | (data[3] & ((1 << 6) - 1));
-  status.currents[15] = (static_cast<uint32_t>(data[3] >> (8-2)) << 6) | data[4];
-  // status.internalResBattery_mOhms = data[5];
-  status.voltage = data[6];
-  // status.internalResBattery_mOhms = data[7];
+  for (int i = 12; i <= 15; i++){
+    status.currents[i] = data[i-12] * kCurrentScalar;
+  }
+  uint8_t vbatByte = data[5]; /* byte 6 */
+  /* scale it to volts */
+  float volts = 0.05f * vbatByte + 4.0f;
+  status.voltage = volts;
+  status.temperature = data[7] * 1.03250836957542 - 67.8564500484966; // byte 8????
 }
