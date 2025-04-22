9c30bd7d28f35a43ce2d5b0c58aa12899c030cff
amadorjosephg <amadorjosephg@gmail.com>
2025-04-21 20:37:01 -0500

    Working, Cntrl backwards, ESp needs manual restart

--- cometbot_ws/build/cometbot_control/prefix_override/sitecustomize.py
+++ cometbot_ws/build/cometbot_control/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/amadorjosephg/CA-Embedded-MiniProject/cometbot_ws/install/cometbot_control'
+    sys.prefix = sys.exec_prefix = '/home/aerobotics/CAN/cometbot_ws/install/cometbot_control'
